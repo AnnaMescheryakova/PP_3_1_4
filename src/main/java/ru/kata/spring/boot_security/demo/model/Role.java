6d15f8aee6e2be83a4f556e076be18542e38643b
AnnaMescheryakova <anna.egorskaya@gmail.com>
2022-05-12 16:56:08 +0600

    commit1

--- src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -3,6 +3,7 @@ package ru.kata.spring.boot_security.demo.model;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "role")
@@ -14,6 +15,14 @@ public class Role implements GrantedAuthority {
 
     @Column(name = "name")
     private String name;
+
+    @Transient
+    @ManyToMany(mappedBy = "roles")
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "users_id")
+    )
+    private List<User> users;
 
     private final static String PREFIX = "ROLE_";
 
@@ -39,6 +48,14 @@ public class Role implements GrantedAuthority {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
     }
 
     @Override
--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -31,7 +31,7 @@ public class User implements UserDetails {
 
     @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "users_roles",
-            joinColumns = @JoinColumn(name = "user_id"),
+            joinColumns = @JoinColumn(name = "users_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id")
     )
     @Column(name = "roles")
